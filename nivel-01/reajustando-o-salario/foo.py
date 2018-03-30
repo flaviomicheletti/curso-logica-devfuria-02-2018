6f7b08786b2171c26af829d8bc96e1dbea82aaa0
Flávio Micheletti <flaviomicheletti@hotmail.com>
2018-03-30 13:43:44 -0300

    reajuste

---
+++ nivel-01/reajustando-o-salario/foo.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+#
+# Reajusta o salário
+#
+def reajustar_salario(salario, reajuste):
+    return salario * reajuste;
+
+# Este é o nosso teste.
+# Sabemos que 1000 * 0.15 = 150, logo...
+assert 150 == reajustar_salario(1000, 0.15), 'reajustar_salario deve retornar 150'
